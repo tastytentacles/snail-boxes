74aa23d9d2e9775251dd808d1cf63e3003f85fed
piQ <marq46.steam@googlemail.com>
2017-02-21 02:22:10 +0000

    mostly broken python geniraton but you get the point

--- shell.py
+++ shell.py
@@ -1,8 +1,67 @@
 # grater shell collection
+from random import choice, randrange
 
 
-SHELLS = {}
+IFLST = [
+ "{}if {} < {}:\n",
+ "{}if {} > {}:\n",
+ "{}if {} == {}:\n",
+ "{}if {} != {}:\n"
+]
+
+CMDLST = [
+ "{}{} = {}\n"
+]
+
+SHELLS = {
+ # 'v0.01':{
+ #  'name': quickname(),
+ #  'cach': {
+ #   'type': "v0.01",
+ #   'filedir': ""
+ #  },
+ #  'macro': [],
+ #  'mem': [randrange(512) for _ in range(16)]
+ # }
+}
+
 SHELLNAMES = [n for n in SHELLS]
+
+def getsnaillist():
+ global BOX
+
+ def f(q):
+  if "_snail.json" in q:
+   return q.replace("_snail.json", "")
+
+ raw = os.listdir(BOX)
+ return [f(n) for n in raw]
+
+def quickname():
+ return "".join([choice(l) for _ in range(8)])
+
+def rngmacro():
+ global IFLST, CMDLST
+
+ def addrule(depth=0):
+  lines = [n for n in macro if macro[depth] != " "]
+
+  t = choice(lines)
+  macro.insert(macro.index(t) + 1, choice(CMDLST).format("".join([" " for _ in range(depth + 1)]), "mem[{}]".format(randrange(16)), "mem[{}]".format(randrange(16))))
+
+ macro = []
+ for _ in range(32):
+  macro.append(choice(IFLST).format("", "mem[{}]".format(randrange(16)), "mem[{}]".format(randrange(16))))
+
+ _ = [addrule() for _ in range(4)]
+
+
+
+ f = open("macro.py", "w")
+ f.writelines(macro)
+ f.close()
+
+ return macro
 
 def rndshell(ver=""):
  """
@@ -10,10 +69,10 @@ def rndshell(ver=""):
   uses the most recent shell format by defalt
  """
  global SHELLS, SHELLNAMES
- 
+
  if not ver in SHELLNAMES:
   s = SHELLS[SHELLNAMES[0]]
  else:
   s = SHELLS[ver]
- 
+
  pass
--- spinner.py
+++ spinner.py
@@ -7,18 +7,6 @@ from random import choice, randrange
 import shell
 
 
-IFLST = [
- "if {} < {}:",
- "",
- "?{}={}",
- "?{}!{}"
-]
-
-CMDLST = [
- ">{}",
- "<{}"
-]
-
 BOX = "box/"
 if len(argv) < 2:
  SNAILCOUNT = 8
@@ -26,39 +14,18 @@ else:
  SNAILCOUNT = int(argv[1])
 
 
-def getsnaillist():
- global BOX
-
- def f(q):
-  if "_snail.json" in q:
-   return q.replace("_snail.json", "")
-
- raw = os.listdir(BOX)
- return [f(n) for n in raw]
-
-def quickname():
- return "".join([choice(l) for _ in range(8)])
-
-def rngmacro():
- global IFLST, CMDLST
-
- macro = {}
- 
- for _ in range(32):
-  macro[choice(IFLST).for]
-
-def shell():
- shell = {
-  'name': quickname(),
-  'cach': {
-   'type': "v0.01",
-   'filedir': ""
-  },
-  'macro': {},
-  'mem': [randrange(512) for _ in range(16)]
- }
-
- return shell
+# def shell():
+#  shell = {
+#   'name': quickname(),
+#   'cach': {
+#    'type': "v0.01",
+#    'filedir': ""
+#   },
+#   'macro': {},
+#   'mem': [randrange(512) for _ in range(16)]
+#  }
+#
+#  return shell
 
 def boxsnail(snail):
  global BOX
@@ -72,12 +39,13 @@ def finalspin():
 
  tshell = {}
  for n in range(SNAILCOUNT):
-  tshell = shell()
+  tshell = shell.rndshell()
 
-  takennames = getsnaillist()
+  takennames = shell.getsnaillist()
   while tshell['name'] in takennames:
-   tshell['name'] = quickname()
+   tshell['name'] = shell.quickname()
 
   boxsnail(tshell)
 
-finalspin()
+# finalspin()
+print(shell.rngmacro())
